31755ee4452fd2d9c99fc196e731009aa5d9dff7
Max <4myself.max@gmail.com>
2022-01-09 21:38:12 +0200

    Add annotation to findAll method in repository

--- src/main/java/com/example/howtoquery/repository/UserRepository.java
+++ src/main/java/com/example/howtoquery/repository/UserRepository.java
@@ -3,10 +3,13 @@ package com.example.howtoquery.repository;
 import com.example.howtoquery.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.Nullable;
 
 import java.util.Date;
 
@@ -22,4 +25,7 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
                                   @Param("createdFrom") Date createdFrom,
                                   @Param("createdTo") Date createdTo,
                                   Pageable pageable);
+
+    @EntityGraph(value = "users.allJoins", type = EntityGraph.EntityGraphType.FETCH)
+    Page<User> findAll(@Nullable Specification<User> specification, Pageable pageable);
 }
